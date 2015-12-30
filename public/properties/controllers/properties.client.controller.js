f83c3db893dab35481c62f4479e2f17713b2348d
alaanzr <alaanzr@gmail.com>
2015-12-30 00:44:39 +0000

    WIP

--- public/properties/controllers/properties.client.controller.js
+++ public/properties/controllers/properties.client.controller.js
@@ -4,7 +4,10 @@ angular.module('properties').controller('PropertyController', ['$scope', '$route
   $scope.createUserProperty = function() {
     var property = new Properties({
       post_code: this.post_code,
-      street_name: this.street_name
+      street_name: this.street_name,
+      property_type: this.property_type,
+      contract_start: this.contract_start,
+      contract_end: this.contract_end
     });
 
     property.$save(function(response) {
