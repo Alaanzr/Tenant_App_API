11afb330cb8201f44c73d382e445760da7cdf78b
Mattia46 <mattia.assogna@gmail.com>
2015-12-31 17:36:39 +0000

    basic mapController setup

--- public/maps/controller/maps.client.controller.js
+++ public/maps/controller/maps.client.controller.js
@@ -11,22 +11,4 @@ angular.module('maps').controller('mapsController', ['$scope', '$routeParams', '
 
 });
 
-  $scope.createUserProperty = function() {
-    var property = new Properties({
-      post_code: this.post_code,
-      street_name: this.street_name,
-      property_type: this.property_type,
-      contract_start: this.contract_start,
-      contract_end: this.contract_end
-    });
-
-    property.$save(function(response) {
-      // $scope.authentication.user.properties.push({post_code: response.post_code,
-      // street_name: response.street_name});
-      $location.path('properties/' + response._id);
-    }, function(errorResponse) {
-      $scope.error = errorResponse.data.message;
-    });
-  };
-
 }]);
