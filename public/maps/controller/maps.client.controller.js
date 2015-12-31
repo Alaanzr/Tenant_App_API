e3ec632644535232328d7ab093fdb27f75278ac5
Mattia46 <mattia.assogna@gmail.com>
2015-12-31 17:42:06 +0000

    added the repo at the bottom of the page as a link

--- public/maps/controller/maps.client.controller.js
+++ public/maps/controller/maps.client.controller.js
@@ -1,14 +1,5 @@
 angular.module('maps').controller('mapsController', ['$scope', '$routeParams', '$location', 'Authentication', 'Properties', function($scope, $routeParams, $location, Authentication, Properties) {
   $scope.authentication = Authentication;
 
-  geolocation.getLocation().then(function(data){
-  coords = {lat:data.coords.latitude, lng:data.coords.longitude};
-  $scope.formData.longitude = parseFloat(coords.lng).toFixed(3);
-  $scope.formData.latitude = parseFloat(coords.lat).toFixed(3);
-  //$scope.formData.htmlverified = "Yep (Thanks for giving us real data!)";
-
-  gservice.refresh($scope.formData.latitude, $scope.formData.longitude);
-
-});
 
 }]);
