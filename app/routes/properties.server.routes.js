62cdb3a70e8ae3016d6f259090b63f6c1a76fc42
Ezzy Elliott <ezzy.elliott@gmail.com>
2016-01-04 15:33:57 +0000

    Added property user api

--- app/routes/properties.server.routes.js
+++ app/routes/properties.server.routes.js
@@ -2,11 +2,11 @@ var properties = require('../../app/controllers/properties.server.controller'), 
 
 module.exports = function(app) {
 
-    // app.route('/properties').post(properties.createUserProperty).get(properties.list);
+    app.route('/properties').post(properties.createUserProperty).get(properties.list);
 
     app.route('/user_properties/:user_id').post(properties.createUserProperty);
 
-    app.route('/properties').post(properties.create).get(properties.list);
+    // app.route('/properties').post(properties.create).get(properties.list);
 
     app.route('/properties/:property_id').get(properties.read).put(properties.update).delete(properties.delete);
 
