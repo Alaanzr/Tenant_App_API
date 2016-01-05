1be65a38783eef95074e68f23c4a1b9fdf6f900d
alaanzr <alaanzr@gmail.com>
2016-01-05 00:29:24 +0000

    Send connection request re-added

--- public/users/controllers/users.client.controller.js
+++ public/users/controllers/users.client.controller.js
@@ -1,8 +1,19 @@
 angular.module('users').controller('UserController', ['$scope', '$http', '$routeParams', 'Authentication', function($scope, $http, $routeParams, Authentication) {
 
+    $scope.authentication = Authentication;
+    var userRecv = $routeParams.user_id;
+    var userSender = $scope.authentication.user.id;
+
     $scope.collectUserDetails = function() {
     $http.get('/users/' + $routeParams.user_id).success(function(data) {
       $scope.data = data;
+    });
+  };
+
+  $scope.sendConnection = function() {
+    $http.post('user_connection/' + userSender + '/' + userRecv).success(function(data) {
+      console.log(data);
+      console.log('connection added');
     });
   };
 }]);
