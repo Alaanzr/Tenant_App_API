7cf90e4c94b0ff27146308aa611c98631a8c0944
Mattia46 <mattia.assogna@gmail.com>
2016-01-05 10:50:45 +0000

    added a basic accept function to create a connnection

--- public/connections/controllers/connections.client.controller.js
+++ public/connections/controllers/connections.client.controller.js
@@ -3,20 +3,25 @@ angular.module('connections').controller('ConnectionController', ['$scope', '$ht
 
   $scope.authentication = Authentication;
 
-  // var userRecv = $routeParams.user_id;
-  var userSender = $scope.authentication.user.id;
-  $scope.requestees = [];
-  var result;
+  var user1 = '568b93173cb22afd0b43e1aa';
+  var user2 = '568b967c3cb22afd0b43e1ab';
 
+
+
+  // var userRecv = $routeParams.user_id;
+  var userRecv = $scope.authentication.user.id;
+  $scope.requestees = [];
+  $scope.connections = [];
   // console.log('receiver, sender:', userRecv, userSender);
 
   $scope.collectUserDetails = function() {
-    $http.get('/users/' + userSender).success(function(data) {
+    $http.get('/users/' + userRecv).success(function(data) {
       $scope.requests_recd = data.requests_recd;
-      $scope.requests_recd.forEach(function(id) {
-        $http.get('/users/' + id).success(function(user) {
+      $scope.requests_recd.forEach(function(user) {
+        $http.get('/users/' + user.id).success(function(user) {
           console.log(user);
           $scope.requestees.push(user);
+          console.log($scope.requestees);
         });
       });
       $scope.data = data;
@@ -25,16 +30,27 @@ angular.module('connections').controller('ConnectionController', ['$scope', '$ht
     // result = $http.get('user_connection/' + userSender + '/' + userRecv);
   };
 
+  $scope.acceptConnection = function() {
+    $http.put('user_connection/' + user2 + '/' + userRecv).success(function(name){
+      console.log(name);
+    });
+    console.log('hello accept');
+  };
+
+  $scope.rejectConnection = function() {
+    console.log('hello reject');
+  };
+
   // $scope.sendConnection = function() {
-  //   $http.post('user_connection/' + userSender + '/' + userRecv).success(function(data) {
-  //     console.log(data);
-  //     console.log('connection added');
-  //   });
-  // };
-  //
-  //  $scope.outstandingConnections = function() {
-  //    if (result == -1) {
-  //      return true;
-  //    }
-  //  };
-}]);
+    //   $http.post('user_connection/' + userSender + '/' + userRecv).success(function(data) {
+      //     console.log(data);
+      //     console.log('connection added');
+      //   });
+      // };
+      //
+      //  $scope.outstandingConnections = function() {
+        //    if (result == -1) {
+          //      return true;
+          //    }
+        //  };
+      }]);
