823597f17889654ad693f6830cc2d740d950b109
alaanzr <alaanzr@gmail.com>
2016-01-06 11:07:56 +0000

    User can edit profile

--- public/main/controllers/main.client.controller.js
+++ public/main/controllers/main.client.controller.js
@@ -1,4 +1,4 @@
-angular.module('main').controller('MainController', ['$scope', 'Authentication', function($scope, Authentication) {
+angular.module('main').controller('MainController', ['$scope', 'Authentication', '$http', '$route', function($scope, Authentication, $http, $route) {
   $scope.authentication = Authentication;
 
   $scope.editStatus = false;
@@ -8,6 +8,22 @@ angular.module('main').controller('MainController', ['$scope', 'Authentication',
   };
 
   $scope.updateProfile = function() {
-    $scope.editStatus = false;
+    var userData = {
+      currentArea: $scope.description,
+      image: $scope.image
+    };
+
+    $http.put('/users/' + $scope.authentication.user.id, userData).success(function(data) {
+      $scope.editStatus = false;
+    });
+    $route.reload();
   };
 }]);
+
+// $scope.acceptConnection = function(index) {
+//   $http.put('user_connection/' + $scope.requests_recd[index].id + '/' + userRecv).success(function(name){
+//     console.log('name:', name);
+//     console.log('end of the accept', $scope.requests_recd[index].id);
+//   });
+//   console.log('hello accept');
+// };
