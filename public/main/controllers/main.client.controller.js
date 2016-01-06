7a35e350ff6aec87108abfaf1ffb09be1d4559e7
alaanzr <alaanzr@gmail.com>
2016-01-06 10:41:59 +0000

    Added option to edit profile

--- public/main/controllers/main.client.controller.js
+++ public/main/controllers/main.client.controller.js
@@ -1,3 +1,13 @@
 angular.module('main').controller('MainController', ['$scope', 'Authentication', function($scope, Authentication) {
   $scope.authentication = Authentication;
+
+  $scope.editStatus = false;
+
+  $scope.changeEditStatus = function() {
+    $scope.editStatus = true;
+  };
+
+  $scope.updateProfile = function() {
+    $scope.editStatus = false;
+  };
 }]);
